71328fde548b726a76f70fb9549e3cafc6201000
Tatsuma Arakawa <tatsuma0742@gmail.com>
2021-06-07 00:08:42 +0900

    ランク実装

--- src/components/gameDisplay/ItTyping.vue
+++ src/components/gameDisplay/ItTyping.vue
@@ -6,7 +6,7 @@
     </div>
 
     <!-- スタート前 -->
-    <div v-if = "startFlag!=true" class="mt-10">
+    <div v-if="startFlag!=true" class="mt-10">
       <v-btn class="startButton mb-20" @click="gameStart">
         <p>クリックでスタート</p>
       </v-btn>
@@ -23,7 +23,7 @@
       <div class="question mb-20">{{ currentWord }}</div>
 
       <!-- 回答欄 -->
-      <div class="input-typing-wrapper mb-20">
+      <div class="mb-20">
         <input id="input-typing" type="text" class="form-control" v-model="typingText">
       </div>
 
@@ -32,15 +32,17 @@
         <div>第{{ currentWordNumber }}問</div>
       </div>
 
-      <!-- タイプ数 -->
-      <div>
-        <div>タイプ数：{{ typeCount }}回</div>
-      </div>
-
       <!-- 経過時間 -->
       <div>
-        <div>経過時間：{{ timer }}秒</div>
+        <div>制限時間：残り{{ timer }}秒</div>
       </div>
+    </div>
+
+    <!-- ゲーム終了時に表示する部分 -->
+    <div v-if="resultFlag">
+      <div>回答数{{ answers }}問</div>
+      <div>ランク{{ rank }}</div>
+      <div>残り時間{{ timer }}</div>
     </div>
   </div>
 </template>
@@ -54,35 +56,46 @@ export default class ItTyping extends Vue {
   // ゲームスタート
   startFlag = false;
 
+  /** 結果 */
+  resultFlag = false
+
   // 入力欄
   typingText = '';
 
-  // ミスタイプ数
-  typeMissCount = 0;
+  /** 回答数 */
+  answers = 0;
 
-  // タイプ数
-  typeCount = 0;
+  /** 制限時間 */
+  TIME = 31
+  timer = this.TIME;
 
-  // 問題解答用
-  questionList = []
-  questionIndex = 0;
-  charIndex = 0;
+  /** ランク */
+  rank = 'E'
 
-  // 経過時間
-  timer = -1; // 最初0秒表示させるため-1に設定
+  rankD = 'D'
+  rankC = 'C'
+  rankB = 'B'
+  rankA = 'A'
+  rankS = 'S'
+
+  scoreD = 3
+  scoreC = 5
+  scoreB = 7
+  scoreA = 9
+  scoreS = 10
 
   /** 問題 */
   words = [
-    'egg',
-    'bag',
-    'rose',
-    'chair',
-    'bat',
-    'fish',
-    'notebook',
-    'pencil',
-    'dog',
-    'desk'
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
   ];
 
   /** 回答後の問題 */
@@ -92,11 +105,32 @@ export default class ItTyping extends Vue {
   private gameStart() {
     this.startFlag = true;
     this.solvedWords = [];
+    this.answers = 0
+    this.typingText = ''
+    this.resultFlag = false
+    this.rank = 'E'
     this.$nextTick(() => {
       document.getElementById('input-typing')?.focus()
     })
-    this.countUp()
-    // window.addEventListener('keypress', this.keyCheck)
+    this.countDown()
+  }
+
+  /** ゲーム終了後 */
+  private result() {
+    this.answers = this.solvedWords.length
+    this.resultFlag = true
+    this.startFlag = false
+    if (this.answers >= this.scoreD) {
+      this.rank = this.rankD
+    } if (this.answers >= this.scoreC) {
+      this.rank = this.rankD
+    } if (this.answers >= this.scoreB) {
+      this.rank = this.rankB
+    } if (this.answers >= this.scoreA) {
+      this.rank = this.rankA
+    } if (this.answers >= this.scoreS) {
+      this.rank = this.rankS
+    }
   }
 
   /** ランダムで問題を出題する */
@@ -119,9 +153,8 @@ export default class ItTyping extends Vue {
       this.solvedWords.push(this.currentWord as never)
       this.typingText = ''
       if(this.words.length == this.solvedWords.length) {
-        this.solvedWords = []
-        this.startFlag = false
-        alert('全問正解')
+        this.result()
+        alert('クリア！！')
       }
       return true
     }
@@ -130,12 +163,19 @@ export default class ItTyping extends Vue {
   }
 
   /** タイマー */
-  countUp(): void {
+  countDown(): void {
     if (!this.startFlag) {
       return
     }
-    this.timer += 1
-    setTimeout(this.countUp, 1000)
+    this.timer -= 1
+    setTimeout(this.countDown, 1000)
+    if (this.words == this.solvedWords) {
+      clearInterval(this.timer)
+    }
+    if (this.timer <= 0) {
+      clearInterval
+      this.result();
+    }
   }
 }
 </script>
@@ -201,10 +241,6 @@ body {
   outline: none;
   border: none;
   text-align: center;
-}
-
-.input-typing-wrapper {
-  border-bottom: 1px solid gray;
 }
 
 .gauge {
